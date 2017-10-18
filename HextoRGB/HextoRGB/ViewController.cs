b5ae215429358a3f7a7c9f5e7a6c3dd1f682f5f0
johnD20 <32309794+johnD20@users.noreply.github.com>
2017-10-18 14:04:32 -0600

    thatshowMUchILOVECHRISTIE

---
+++ HextoRGB/HextoRGB/ViewController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+using UIKit;
+
+namespace HextoRGB
+{
+    public partial class ViewController : UIViewController
+    {
+        protected ViewController(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+
+            convertButton.TouchUpInside += ConvertButton_TouchUpInside;
+        }
+
+        void ConvertButton_TouchUpInside(object sender, EventArgs e)
+        {
+            //turns hex value and Individual RGB values into strings
+            string hexValue = hexValueTextField.Text;
+            string redHexValue = hexValue.Substring(0, 2);
+            string greenHexValue = hexValue.Substring(2, 2);
+            string blueHexValue = hexValue.Substring(4, 2);
+
+            //changes values of strings into integers
+            int redValue = int.Parse(redHexValue, System.Globalization.NumberStyles.HexNumber);
+            int greenValue = int.Parse(greenHexValue, System.Globalization.NumberStyles.HexNumber);
+            int blueValue = int.Parse(blueHexValue, System.Globalization.NumberStyles.HexNumber);
+
+            //sends each value to their respective label
+            redValueLabel.Text = redValue.ToString();
+            greenValueLabel.Text = greenValue.ToString();
+            blueValueLabel.Text = blueValue.ToString();
+
+            //changes background colour to the hex value entered by user
+            colorView.BackgroundColor = UIColor.FromRGB(redValue, greenValue, blueValue);
+
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
